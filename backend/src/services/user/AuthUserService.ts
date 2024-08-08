5c52645a6414aad4cd7bee19d41a7c3780e49826
miguel <weigertmiguel@hotmail.com>
2024-08-08 15:41:37 -0300

    Autenticação com token JWT, Logando e gerando o token.

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthRequest {
   email: string;
@@ -28,8 +29,25 @@ class AuthUserService {
     }
 
     //gerar um token JWT e devolver os dados do usuário como id, name e email.
+    //Se deu tudo  certo iremos gerar o token JWT para o usuario
+    const token = sign({
+      name: user.name,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+    {
+        subject: user.id,
+        expiresIn: '30d'
+    }
+);
 
-    return { ok: true };
+
+    return { 
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        token: token
+     };
   }
 }
 
