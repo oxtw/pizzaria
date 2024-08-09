3c53b7b8bb63fd0584e46db84129aebaf646c2fa
miguel <weigertmiguel@hotmail.com>
2024-08-09 13:01:34 -0300

    Rota Produtos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,6 +7,8 @@ import { DetailUserController } from "./controllers/user/DetailUserController";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
+import { CreateProductController } from "./controllers/product/CreateProductController";
+
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
@@ -20,8 +22,16 @@ router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 //-- ROTAS CATEGORY --
-router.post("/category", isAuthenticated, new CreateCategoryController().handle);
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
 
 router.get("/category", isAuthenticated, new ListCategoryController().handle);
+
+// -- ROTAS PRODUCT --
+
+router.post('/product', isAuthenticated, new CreateProductController().handle);
 
 export { router };
