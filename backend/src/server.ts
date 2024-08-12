b8227e0ef7a67c4a2db698e412bd653f80f1430a
miguel <weigertmiguel@hotmail.com>
2024-08-12 14:52:23 -0300

    Cadastrando produtos

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import cors from "cors";
+import path from "path";
 
 import { router } from "./routes";
 
@@ -9,6 +10,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use(router);
+
+//função para encontrar um arquivo
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 //Middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
