c9fa88f615391ac7ae3adaab06e9bfab01f2e88d
miguel <weigertmiguel@hotmail.com>
2024-08-12 15:26:27 -0300

    Filtrando produtos por categoria

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,18 +1,23 @@
 import { Router } from "express";
 import multer from "multer";
 
+//user
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
+//category
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
+//product
 import { CreateProductController } from "./controllers/product/CreateProductController";
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 
+//middleware auth
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
-import uploadConfig from './config/multer';
+import uploadConfig from "./config/multer";
 
 const router = Router();
 const upload = multer(uploadConfig.upload("./tmp"));
@@ -26,13 +31,24 @@ router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 //-- ROTAS CATEGORY --
-router.post("/category", isAuthenticated, new CreateCategoryController().handle
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
 );
 
 router.get("/category", isAuthenticated, new ListCategoryController().handle);
 
 // -- ROTAS PRODUCT --
 
-router.post('/product', isAuthenticated, upload.single("file"), new CreateProductController().handle);
+router.post(
+  "/product",
+  isAuthenticated,
+  upload.single("file"),
+  new CreateProductController().handle
+);
+
+//rota para filtrar produtos por categoria.
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle);
 
 export { router };
