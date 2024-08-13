094eb7c4d39a43d334e9aef7c1f11a0d96ccad4d
miguel <weigertmiguel@hotmail.com>
2024-08-13 20:00:19 -0300

    Função para finalizar Ordem de serviço, finalizando o backend da aplicação.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -22,6 +22,7 @@ import { RemoveItemController } from "./controllers/order/RemoveItemController";
 import { SendOrderController } from "./controllers/order/SendOrderController";
 import { ListOrdersController } from "./controllers/order/ListOrdersController";
 import { DetailOrderController } from "./controllers/order/DetailOrderController";
+import { FinishOrderController } from "./controllers/order/FinishOrderController";
 
 //middleware auth
 import { isAuthenticated } from "./middlewares/isAuthenticated";
@@ -66,5 +67,8 @@ router.put("/order/send", isAuthenticated, new SendOrderController().handle);
 router.get("/orders", isAuthenticated , new ListOrdersController().handle);
 
 router.get("/order/detail", isAuthenticated, new DetailOrderController().handle);
+
+router.put("/order/finish", isAuthenticated, new FinishOrderController().handle);
+
 
 export { router };
