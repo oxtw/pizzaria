593c24ed313b71f61750377b3439d0719bab2466
miguel <weigertmiguel@hotmail.com>
2024-08-13 12:59:31 -0300

    Enviando o pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -19,6 +19,8 @@ import { CreateOrderController } from "./controllers/order/CreateOrderController
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
+import { SendOrderController } from "./controllers/order/SendOrderController";
+
 //middleware auth
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
@@ -69,5 +71,7 @@ router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
 router.post("/order/add", isAuthenticated, new AddItemController().handle);
 
 router.delete("/order/remove", isAuthenticated, new RemoveItemController().handle);
+
+router.put("/order/send", isAuthenticated, new SendOrderController().handle);
 
 export { router };
