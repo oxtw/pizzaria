dcdf50827b2afca849ef64105765b66091258ffb
miguel <weigertmiguel@hotmail.com>
2024-08-22 16:40:00 -0300

    criando página de login, começando estilização com o sass

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,42 @@
-import styles from '@/app/page.module.scss' 
+import styles from "@/app/page.module.scss";
+import logoImg from "../../public/logo.svg";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className={styles.teste}>teste teste</h1>
-    </main>
+    <>
+      <div className={styles.containerCenter}>
+        {/*Logo*/}
+        <Image src={logoImg} alt="Logo da pizzaria" />
+
+        {/* Formulário de login */}
+        <section className={styles.login}>
+          <form>
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Digite seu e-mail..."
+              className={styles.input}
+            />
+
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="**********"
+              className={styles.input}
+            />
+
+            <button type="submit">Acessar</button>
+
+            <Link href="/signup" className={styles.text}>
+              Não possui uma conta? Cadastre-se.
+            </Link>
+          </form>
+        </section>
+      </div>
+    </>
   );
 }
