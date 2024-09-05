352568f179a3f0639b7ff8ed9635595dd2fddd66
miguel <weigertmiguel@hotmail.com>
2024-09-05 12:10:00 -0300

    Cadastrando categoria

--- frontend/src/app/dashboard/category/page.tsx
+++ frontend/src/app/dashboard/category/page.tsx
@@ -1,15 +1,37 @@
 import styles from "./styles.module.scss";
 import { Button } from "@/app/dashboard/components/button/";
+import { api } from "@/services/api";
+import { getCookieServer } from "@/lib/cookieServer";
+import { redirect } from "next/navigation";
 
 export default function Category() {
-  
-    async function handleRegisterCategory(){
-        "use server"
+  async function handleRegisterCategory(formData: FormData) {
+    "use server";
 
-        console.log('teste')
+    const token = getCookieServer();
+    const name = formData.get("name");
+
+    if (!name) {
+      return;
     }
 
-    return (
+    const data = {
+      name: name,
+    };
+
+    await api.post("/category", data, {
+        headers:{
+            Authorization:`Bearer ${token}`
+        }
+    }).catch((err) => {
+      console.log(err);
+      return;
+    });
+
+    redirect('/dashboard')
+  }
+
+  return (
     <main className={styles.container}>
       <h1>Nova Categoria</h1>
       <form action={handleRegisterCategory} className={styles.form}>
