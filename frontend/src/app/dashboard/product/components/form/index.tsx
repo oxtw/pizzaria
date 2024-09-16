e8ee345e1c0657309da065c8b952c1a606cec000
miguel <weigertmiguel@hotmail.com>
2024-09-16 13:47:10 -0300

    Preview produto

--- frontend/src/app/dashboard/product/components/form/index.tsx
+++ frontend/src/app/dashboard/product/components/form/index.tsx
@@ -4,6 +4,7 @@ import { ChangeEvent, useState } from "react";
 import styles from "../form/styles.module.scss";
 import { UploadCloud } from "lucide-react";
 import Image from "next/image";
+import { Button } from "@/app/dashboard/components/button";
 
 export function Form() {
   const [image, setImage] = useState<File>();
@@ -40,17 +41,45 @@ export function Form() {
             onChange={handleFile}
           />
 
-
           {previewImage && (
-            <Image 
-            alt="Imagem de preview"
-            src={previewImage}
-            className={styles.preview}
-            fill={true}
-            priority={true}
+            <Image
+              alt="Imagem de preview"
+              src={previewImage}
+              className={styles.preview}
+              fill={true}
+              quality={100}
+              priority={true}
             />
           )}
         </label>
+
+        <select name="category">
+          <option key={1} value={1}>
+            Pizzas
+          </option>
+          <option key={1} value={1}>
+            Massas
+          </option>
+        </select>
+        <input
+          type="text"
+          name="name"
+          placeholder="Digite o nome do produto..."
+          className={styles.input}
+        />
+        <input
+          type="text"
+          name="price"
+          placeholder="Preço do produto..."
+          className={styles.input}
+        />
+        <textarea
+          name="description"
+          placeholder="Digite a descrição do produto..."
+          className={styles.input}
+        />
+
+        <Button name="Cadastrar produto" />
       </form>
     </main>
   );
