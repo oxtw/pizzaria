8bf592b4e101060c04f34c894034f8b7d8aed884
miguel <weigertmiguel@hotmail.com>
2024-09-18 13:29:18 -0300

    Buscando pedidos

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,6 +1,28 @@
 import { Orders } from "./components/orders";
+import { api } from "@/services/api";
+import { getCookieServer } from "@/lib/cookieServer";
+import { OrderProps } from "@/lib/order.type";
 
-export default function Dashboard() {
+async function getOrders(): Promise<OrderProps[] | []> {
+  try {
+    const token = getCookieServer();
+    const response = await api.get("/orders", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    return response.data || [];
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
+
+export default async function Dashboard() {
+  const orders = await getOrders();
+  console.log(orders);
+
   return (
     <>
       <Orders />
