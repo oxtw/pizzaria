3bd1ff396fa24e197dd627a4d69929027f7f30b5
miguel <weigertmiguel@hotmail.com>
2024-09-24 12:53:05 -0300

    Criando modal do pedido

---
+++ frontend/src/app/dashboard/components/modal/index.tsx
@@ -0,0 +1,41 @@
+import styles from "./styles.module.scss";
+import { X } from "lucide-react";
+
+export function ModalOrder() {
+  return (
+    <dialog className={styles.dialogContainer}>
+      <section className={styles.dialogContent}>
+        <button className={styles.dialogButton}>
+          <X size={40} color="#FF3f4b" />
+        </button>
+
+        <article className={styles.container}>
+          <h2>Detalhes do pedido</h2>
+          <span className={styles.table}>
+            Mesa <b>36</b>
+          </span>
+
+          <section className={styles.item}>
+            <span>
+              1 - <b>Pizza catupiry</b>
+            </span>
+            <span className={styles.description}>
+                Pizza de frango com catupiry
+            </span>
+          </section>
+          
+          <section className={styles.item}>
+            <span>
+              1 - <b>Pizza catupiry</b>
+            </span>
+            <span className={styles.description}>
+                Pizza de frango com catupiry
+            </span>
+          </section>
+
+          <button className={styles.submitButton}>Concluir pedido</button>
+        </article>
+      </section>
+    </dialog>
+  );
+}
--- frontend/src/app/dashboard/components/orders/index.tsx
+++ frontend/src/app/dashboard/components/orders/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./styles.module.scss";
 import { RefreshCw } from "lucide-react";
 import { OrderProps } from "@/lib/order.type";
+import { ModalOrder } from "@/app/dashboard/components/modal";
 
 interface Props {
   orders: OrderProps[];
@@ -8,26 +9,26 @@ interface Props {
 
 export function Orders({ orders }: Props) {
   return (
-    <main className={styles.container}>
-      <section className={styles.containerHeader}>
-        <h1>Ultimos pedidos</h1>
-        <button>
-          <RefreshCw size={24} color="#3fffa3" />
-        </button>
-      </section>
-
-      <section className={styles.listOrders}>
-        {orders.map((order) => (
-          <button 
-            key={order.id} 
-            className={styles.orderItem}>
-
-            <div className={styles.tag}></div>
-            <span>Mesa {order.table}</span>
-            
+    <>
+      <main className={styles.container}>
+        <section className={styles.containerHeader}>
+          <h1>Ultimos pedidos</h1>
+          <button>
+            <RefreshCw size={24} color="#3fffa3" />
           </button>
-        ))}
-      </section>
-    </main>
+        </section>
+
+        <section className={styles.listOrders}>
+          {orders.map((order) => (
+            <button key={order.id} className={styles.orderItem}>
+              <div className={styles.tag}></div>
+              <span>Mesa {order.table}</span>
+            </button>
+          ))}
+        </section>
+      </main>
+
+      <ModalOrder/>
+    </>
   );
 }
