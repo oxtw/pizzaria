6cf113b720d698d98c934d10bda71e0e8c7bb2f3
miguel <weigertmiguel@hotmail.com>
2024-09-25 13:52:08 -0300

    Ajustando dashboard

--- frontend/src/app/dashboard/components/orders/index.tsx
+++ frontend/src/app/dashboard/components/orders/index.tsx
@@ -7,33 +7,47 @@ import styles from "./styles.module.scss";
 import { RefreshCw } from "lucide-react";
 import { OrderProps } from "@/lib/order.type";
 import { ModalOrder } from "@/app/dashboard/components/modal";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface Props {
   orders: OrderProps[];
 }
 
 export function Orders({ orders }: Props) {
-  const {isOpen, onRequestOpen } = use(OrderContext);
+  const { isOpen, onRequestOpen } = use(OrderContext);
 
-  async function handleDetailOrder(order_id: string){
-   await onRequestOpen(order_id);
+  const router = useRouter();
+
+  async function handleDetailOrder(order_id: string) {
+    await onRequestOpen(order_id);
   }
+
+  function handleRefresh(){
+    router.refresh();
+    toast.success("Pedidos atualizados com sucesso!")
+  }
+
   return (
     <>
       <main className={styles.container}>
         <section className={styles.containerHeader}>
           <h1>Ultimos pedidos</h1>
-          <button>
+          <button onClick={handleRefresh}>
             <RefreshCw size={24} color="#3fffa3" />
           </button>
         </section>
 
         <section className={styles.listOrders}>
+          {orders.length === 0 && (
+            <span className={styles.emptyItem}>Nenhum pedido aberto no momento...</span>
+          )}
+
           {orders.map((order) => (
-            <button 
-            key={order.id} 
-            className={styles.orderItem}
-            onClick={() => handleDetailOrder(order.id)}
+            <button
+              key={order.id}
+              className={styles.orderItem}
+              onClick={() => handleDetailOrder(order.id)}
             >
               <div className={styles.tag}></div>
               <span>Mesa {order.table}</span>
@@ -42,7 +56,7 @@ export function Orders({ orders }: Props) {
         </section>
       </main>
       {/* Se isOpen = true, executar o ModalOrder*/}
-      {isOpen && <ModalOrder /> }
+      {isOpen && <ModalOrder />}
     </>
   );
 }
